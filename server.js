3969cde525cbffadde7558b2ab86e8e725c2c5b0
Kevin Luo <kvn.luo@gmail.com>
2020-10-15 09:54:11 -0400

    add server.js notes

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/notes', (req,res) => {
 })
 
 app.get('/api/notes', (req,res) => {
-  console.log('hello');
+  // send parsed json db to be rendered on notes.html
   fs.readFile('db.json', (err,data) => {
     let jsonData = JSON.parse(data);
     res.json(jsonData);
@@ -23,6 +23,7 @@ app.get('/api/notes', (req,res) => {
   })
 })
 
+// placed this route handler after all other get requests otherwise app wouldn't work
 app.get('*', (req,res) => {
   res.sendFile(path.join(__dirname,"/public/index.html"));
 })
@@ -31,11 +32,16 @@ app.post('/api/notes', function(req,res) {
   let postStuff = req.body;
 
   fs.readFile('db.json', (err,data) => {
+    // parse json data in db.json
     let jsonData = JSON.parse(data);
+    // create 'id' property and set it equal to the length of the array before adding the new note in
     postStuff.id = jsonData.length;
+    // add note to json db
     jsonData.push(postStuff);
+    // overwrite json db with new array
     fs.writeFile('db.json', JSON.stringify(jsonData), (err,data) => {
       if (err) throw err;
+      // redirect to get request route handler
       res.redirect('/api/notes');
     })
   });
@@ -45,15 +51,18 @@ app.delete("/api/notes/:id", (req,res) => {
   let noteId = req.params.id;
   fs.readFile('db.json', (err,data) => {
     let jsonData = JSON.parse(data);
+    // remove the note with id equal to the route parameter id
     let filteredData = jsonData.filter(note => {
       return note.id !== parseInt(noteId);
     })
+    // update 'id' properties for all notes
     let i=0;
     let updatedIds = filteredData.map(note => {
       note.id = i;
       i++;
       return note;
     })
+    // overrite json db with updated note array
     fs.writeFile('db.json', JSON.stringify(updatedIds), (err,data) => {
       if (err) throw err;
       res.send('note deleted');
